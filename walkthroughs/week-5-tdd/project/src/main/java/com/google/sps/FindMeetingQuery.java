91daf0ba6852ba918e44dc0be94c55d567c47628
Emma Hogan <emma.hogan314@gmail.com>
2020-12-22 02:07:38 +0000

    Add current progress

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,60 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Arrays;
+import java.util.*;
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
+    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+
+        Collection<String> requiredAttendees = request.getAttendees();
+        int requestedDuration = (int) request.getDuration();
+
+        Collection<TimeRange> possibleTimes = new ArrayList<TimeRange>();
+
+        TimeRange eventTime;
+        Collection<String> eventAttendees;
+        boolean isAttendeeOverlap;
+
+        int potentialStartTime = TimeRange.START_OF_DAY;
+        TimeRange potentialTimeRange = TimeRange.fromStartDuration(
+            potentialStartTime, requestedDuration
+        );
+        Collection<Event> sortedEvents = sortEventsByStart(events);
+
+        for (Event event : sortedEvents) {
+            //if overlaps with time-range and has attendees in common, not an option
+            eventTime = event.getWhen();
+            eventAttendees = event.getAttendees();
+            isAttendeeOverlap = isAttendeeOverlap(requiredAttendees, eventAttendees);
+            if (potentialTimeRange.overlaps(eventTime) && isAttendeeOverlap) {
+                // this timeframe does not work
+                // adjust to next potential timeframe
+                potentialStartTime = eventTime.end();
+                potentialTimeRange = TimeRange.fromStartDuration(potentialStartTime, requestedDuration);
+            } else if (isAttendeeOverlap) {
+                // all times from potential start time up to this event are free for a meeting
+                possibleTimes.add(TimeRange.fromStartEnd(potentialStartTime, eventTime.end(), false));
+                // adjust to next potential timeframe
+                potentialStartTime = eventTime.end();
+                potentialTimeRange = TimeRange.fromStartDuration(potentialStartTime, requestedDuration);
+            }
+        }
+        return possibleTimes;
+    }
+
+    private boolean isAttendeeOverlap(Collection<String> event1Attendees, Collection<String> event2Attendees) {
+        for (String attendee : event2Attendees) {
+            if (event1Attendees.contains(attendee)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Collection<Event> sortEventsByStart(Collection<Event> events) {
+        Collections.sort(events, 
+            (e1, e2) -> e1.getWhen().start().compareTo(e2.getWhen().start()));
+        return events;
+    }
 }
